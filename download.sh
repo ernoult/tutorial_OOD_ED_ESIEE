75eba3c4b434f88b8dfb8816253da4f5003e1255
ERNOULT <ernoult@mbp-de-ernoult.home>
2021-11-09 12:11:23 +0100

    added bayes by backprop to the notebook

--- download.sh
+++ download.sh
@@ -2,13 +2,14 @@
 mkdir datasets
 cd datasets
 # Imagenet (resize)
-wget https://www.dropbox.com/s/kp3my3412u5k9rl/Imagenet_resize.tar.gz
+wget --no-check-certificate  https://www.dropbox.com/s/kp3my3412u5k9rl/Imagenet_resize.tar.gz
 tar -xvzf Imagenet_resize.tar.gz
 
 
 # Models
+cd ..
 mkdir pre_trained
 cd pre_trained
 # DenseNet-BC trained on CIFAR-10
-wget https://www.dropbox.com/s/wr4kjintq1tmorr/densenet10.pth.tar.gz
+wget --no-check-certificate https://www.dropbox.com/s/wr4kjintq1tmorr/densenet10.pth.tar.gz
 tar -xvzf densenet10.pth.tar.gz
